a09a6ab918fdac83d3b25a3c1dd47a6548547b65
YDC <miniming1026@naver.com>
2022-05-14 23:25:18 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+        let gnb = document.querySelectorAll("#gnb > li")
+        let gnbElement = document.querySelector("#gnb")
+
+        for (let i = 0; i < gnb.length; i++) {
+            gnb[i].addEventListener("mouseover",() => {
+                gnbElement.classList.add("on")
+            })
+        }
+
+        let headerElement = document.querySelector("#header")
+
+        header.addEventListener("mouseout",(e) =>{
+            if (e.target.id == "gnb" ){
+                gnbElement.classList.remove("on")
+            }
+        })
+  </script>
